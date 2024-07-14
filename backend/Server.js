46d88a9b681f869e52ad84aec83aac399269ca35
apar@interviewer.ai <apar@interviewer.ai>
2024-07-14 21:44:43 +0530

    server side rendering with node

--- backend/Server.js
+++ backend/Server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -6,6 +7,17 @@ const PORT = 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./views/index.html', 'utf-8',(err, data) => {
+       if(err) {
+         console.log('somethig went wrong', err);
+         return res.status(500).send('Oops something went wrong');
+       }
+       return res.send(data);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
